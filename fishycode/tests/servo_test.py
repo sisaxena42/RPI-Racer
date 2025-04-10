48331ba2bc445d82295def6e61a02b5f132d11ef
sisaxena42 <sisaxena@ad.unc.edu>
2025-04-10 16:47:16 -0400

    some changes didnt pass

---
+++ fishycode/tests/servo_test.py
@@ -0,0 +1,14 @@
+import serial 
+import time
+
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout = 1.0)
+time.sleep(3)
+ser.reset_input_buffer()
+print('Serial Connection OK')
+
+try:
+    while True:
+        
+except KeyboardInterrupt:
+    print("connection severed")
+    ser.close()
