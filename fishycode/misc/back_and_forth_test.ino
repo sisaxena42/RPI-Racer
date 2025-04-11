e4faff4d8ce6a10554d3a721b7f1f7c26ed8aa50
sisaxena42 <sisaxena@ad.unc.edu>
2025-04-10 20:58:47 -0400

    motor tests

--- fishycode/misc/back_and_forth_test.ino
+++ fishycode/misc/back_and_forth_test.ino
@@ -25,3 +25,10 @@ void run_esc_test() {
   set_esc_power(40); delay(500);
   set_esc_power(0);  delay(500);
 }
+void set_esc_power (int power){
+  power = constrain(power, -100, 100);
+  int signal_min = 1050;
+  int signal_max = 1950;
+  int signal_output = map(power, -100, 100, signal_min, signal_max); //map(value, fromLow, fromHigh, toLow, toHigh)
+  esc_1.writeMicroseconds(signal_output);
+}
