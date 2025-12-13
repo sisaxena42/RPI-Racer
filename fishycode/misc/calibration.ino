20723bc99bb7a4e344106ab1b2a08f8b341c0386
Your Name <you@example.com>
2025-12-13 16:00:16 -0500

    also updated callibration

--- fishycode/misc/calibration.ino
+++ fishycode/misc/calibration.ino
@@ -1,20 +1,25 @@
 #include <Servo.h>
 
 Servo esc;
+Servo servo;
 
 const int ESC_PIN  = 9;
+const int SERVO_PIN = 14;
 
 // Your measured values from the receiver:
-const int ESC_MIN  = 1235;  // reverse
+const int ESC_MIN  = 1050;  // reverse
 const int ESC_NEUT = 1475;  // neutral
-const int ESC_MAX  = 1736;  // forward
+const int ESC_MAX  = 1530;  // forward
 
 void setup() {
   Serial.begin(19200);
   esc.attach(ESC_PIN);
+  servo.attach(SERVO_PIN);
+  
 
   // Start at NEUTRAL
   esc.writeMicroseconds(ESC_NEUT);
+  servo.writeMicroseconds(1500);
   Serial.println("Send: NEUTRAL");
 }
 
@@ -29,11 +34,21 @@ void loop() {
     }
     if (c == 'n') {
       esc.writeMicroseconds(ESC_NEUT);
+      servo.writeMicroseconds(1500);
       Serial.println("Send: NEUTRAL");
     }
-    if (c == 'r') {
+    if (c == 'b') {
       esc.writeMicroseconds(ESC_MIN);
       Serial.println("Send: FULL REVERSE");
     }
+    if (c == 'l') {
+      servo.writeMicroseconds(1300);
+      Serial.println("Send: TURN LEFT");
+    }
+    if (c == 'r') {
+       servo.writeMicroseconds(1800);
+      Serial.println("Send: TURN RIGHT");
+    }
+    
   }
 }
